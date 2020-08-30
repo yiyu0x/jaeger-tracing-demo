3f0aa1384611aa60a702515235f366f079f3ae55
yiyu0x <yiyu0x@icloud.com>
2020-08-30 18:42:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# jaeger_tracing_demo
+
+Running:
+
+`docker-compose up`
+
+Visit app:
+
+`http://localhost/app/`
+
+or
+
+`http://localhost/app/<any-string>`
+
+Visit Jaeger:
+
+`http://localhost:16686`
+
+Console will show log and traceId use to search span in Jaeger.
---
+++ app.js
@@ -0,0 +1,30 @@
+const tracingName = 'express-endpoint'
+const jaegerHost = process.env.JAEGER_HOST || 'jaeger'
+const tracer = require('./src/tracer')(tracingName, jaegerHost)
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+const morgan = require('morgan')
+morgan.token('traceId', function (req, res) { return req.headers['x-b3-traceid'] })
+const loggerFormat = '[:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":traceId"'
+app.use(morgan(loggerFormat))
+
+app.get('/app', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.get('/app/:argv', (req, res) => {
+	res.send(req.params.argv)
+	console.log({
+		traceId: tracer.getCurrentSpan().spanContext.traceId,
+		spanId: tracer.getCurrentSpan().spanContext.spanId,
+		parentSpanId: tracer.getCurrentSpan().parentSpanId
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Listening at http://localhost:${port}`)
+})
+
