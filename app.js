1ca29b88d8924cd070c528a0a3f17575f70c6dd0
yiyu0x <yiyu0x@icloud.com>
2020-09-12 18:58:28 +0800

    jarger-clinet

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-const tracingName = 'express-endpoint'
-const jaegerHost = process.env.JAEGER_HOST || 'jaeger'
-const tracer = require('./src/tracer')(tracingName, jaegerHost)
+const { default: middleware } = require("express-opentracing")
+const tracer = require('./src/tracer')
+const path = require('path');
 
 const express = require('express')
 const app = express()
+app.use(middleware({ tracer: tracer }))
 const port = 3000
 
 const morgan = require('morgan')
-morgan.token('traceId', function (req, res) { return req.headers['x-b3-traceid'] })
+morgan.token('traceId', function (req, res) { return req.headers['uber-trace-id'].split(':')[0] })
 const loggerFormat = '[:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":traceId"'
 app.use(morgan(loggerFormat))
 
+app.use('/app/dist', express.static(__dirname + '/src/public'));
+
 app.get('/app', (req, res) => {
-	res.send('Hello World!')
+	res.send(`Hello World!</br>
+	traceId: ${req.headers['uber-trace-id'].split(':')[0]}</br>
+	Visit Jaeger at <a href="http://localhost:16686">jaeger</a>`)
+})
+
+app.get('/app/page', (req, res) => {
+	res.sendFile(path.join(__dirname + '/src/dist/index.html'));
 })
 
 app.get('/app/:argv', (req, res) => {
-	res.send(req.params.argv)
-	console.log({
-		traceId: tracer.getCurrentSpan().spanContext.traceId,
-		spanId: tracer.getCurrentSpan().spanContext.spanId,
-		parentSpanId: tracer.getCurrentSpan().parentSpanId
-	})
+	res.send(`req.params.argv: ${req.params.argv}</br>
+	traceId: ${req.headers['uber-trace-id'].split(':')[0]}</br>
+	Visit Jaeger at <a href="http://localhost:16686">jaeger</a>`)
 })
+
+
 
 app.listen(port, () => {
 	console.log(`Listening at http://localhost:${port} in container.`)
--- src/tracer.js
+++ src/tracer.js
@@ -1,48 +1,18 @@
-'use strict';
+var initTracer = require('jaeger-client').initTracer;
 
-const opentelemetry = require('@opentelemetry/api');
-const { NodeTracerProvider } = require('@opentelemetry/node');
-const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
-// I am using Jaeger as exporter
-const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
-
-// This is not mandatory, by default httptrace propagation is used
-// but it is not well supported by the PHP ecosystem and I have
-// a PHP service to instrument. I discovered B3 is supported
-// form all the languages I where intrumenting
-const { B3Propagator } = require('@opentelemetry/core');
-
-module.exports = (serviceName, jaegerHost) => {
-  // A lot of those plugins are automatically loaded when you install them
-  // So if you do not use express for example you do not have to enable all
-  // those plugins manually. But Express is not auto enabled so I had to add them
-  // all
-  const provider = new NodeTracerProvider({
-    plugins: {
-      express: {
-        enabled: true,
-        path: '@opentelemetry/plugin-express',
-      },
-      // http: {
-      //   enabled: true,
-      //   path: '@opentelemetry/plugin-http',
-      // },
-    }
-  });
-
-  // Here is where I configured the exporter, setting the service name
-  // and the jaeger host. The logger is helpful to track errors from the
-  // exporter itself
-  let exporter = new JaegerExporter({
-    serviceName: serviceName,
-    host: jaegerHost
-  });
-
-  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-  provider.register({
-    propagator: new B3Propagator(),
-  });
-  // Set the global tracer so you can retrieve it from everywhere else in the
-  // app
-  return opentelemetry.trace.getTracer(serviceName);
+// See schema https://github.com/jaegertracing/jaeger-client-node/blob/master/src/configuration.js#L37
+var config = {
+  serviceName: 'my-awesome-service',
+  reporter: {
+    agentHost: 'jaeger'
+  },
 };
+var options = {
+  tags: {
+    'foo': 'bar',
+  },
+  // metrics: metrics,
+  // logger: logger,
+};
+var tracer = initTracer(config, options);
+module.exports = tracer
